a1c6601f7cda557a35fe58706917244a42d587d5
Felipe Tadeu <felipetadeu.dev@gmail.com>
2024-10-01 20:55:43 -0300

    novo teste tentando passar direto

--- install.sh
+++ install.sh
@@ -22,46 +22,39 @@ run_silently() {
     "$@" > /dev/null 2>&1
 }
 
-# Função para lidar com erros
-handle_error() {
-    echo "Erro: $1"
-    exit 1
-}
-
 # Instalação inicial das dependências básicas, incluindo figlet
-
-run_silently sudo apt-get update || handle_error "Falha ao atualizar os pacotes"
-run_silently sudo apt-get install -y apt-transport-https ca-certificates curl gnupg lsb-release figlet || handle_error "Falha ao instalar dependências básicas"
+run_silently sudo apt-get update
+run_silently sudo apt-get install -y apt-transport-https ca-certificates curl gnupg lsb-release figlet
 
 # Agora que figlet está instalado, exibir o texto "MEMBER AREA" em formato grande
-figlet "MEMBRIUMWL"
+figlet "MEMBRIUM WL"
 animate_dots "Preparando" 30 &
 
 # Instalação automatizada do Docker
-sudo mkdir -p /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg || handle_error "Falha ao baixar ou processar a chave GPG do Docker"
-sudo chmod a+r /etc/apt/keyrings/docker.gpg
+run_silently sudo mkdir -p /etc/apt/keyrings
+run_silently curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+run_silently sudo chmod a+r /etc/apt/keyrings/docker.gpg
 
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
-run_silently sudo apt-get update || handle_error "Falha ao atualizar os pacotes após adicionar o repositório do Docker"
-run_silently sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin || handle_error "Falha ao instalar o Docker"
+run_silently sudo apt-get update
+run_silently sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
 
 # Instalação do Docker Compose mais recente
 DOCKER_COMPOSE_VERSION=$(curl -s https://api.github.com/repos/docker/compose/releases/latest | grep 'tag_name' | cut -d'"' -f4)
-sudo curl -L "https://github.com/docker/compose/releases/download/${DOCKER_COMPOSE_VERSION}/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose || handle_error "Falha ao baixar o Docker Compose"
-sudo chmod +x /usr/local/bin/docker-compose
+run_silently sudo curl -L "https://github.com/docker/compose/releases/download/${DOCKER_COMPOSE_VERSION}/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+run_silently sudo chmod +x /usr/local/bin/docker-compose
 wait
 
 # Download do docker-compose.yml
 animate_dots "Baixando" 5 &
-curl -L https://raw.githubusercontent.com/Celipi/member_area/main/docker-compose.yml -o docker-compose.yml || handle_error "Falha ao baixar o arquivo docker-compose.yml"
+run_silently curl -L https://raw.githubusercontent.com/Celipi/member_area/main/docker-compose.yml -o docker-compose.yml
 
 # Criar a rede traefik_proxy (se não existir)
 if ! docker network inspect traefik_proxy >/dev/null 2>&1; then
-    sudo docker network create traefik_proxy || handle_error "Falha ao criar a rede traefik_proxy"
+    run_silently sudo docker network create traefik_proxy
 fi
 
 # Criar o diretório letsencrypt (para os certificados SSL)
@@ -82,7 +75,7 @@ sed -i "s/--certificatesresolvers.letsencrypt.acme.email=seu_email@example.com/-
 
 # Iniciar a aplicação com o Docker Compose
 animate_dots "Iniciando" 15 &
-sudo docker-compose up -d || handle_error "Falha ao iniciar os contêineres com Docker Compose"
+run_silently sudo docker-compose up -d
 wait
 
 echo -e "\nInstalação concluída! Acesse seu aplicativo em https://$DOMINIO"
