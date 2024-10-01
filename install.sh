6c2e94825d3203611ed0f2dfed5723227c619c67
Felipe Tadeu <felipetadeu.dev@gmail.com>
2024-10-01 17:14:43 -0300

    testando

--- install.sh
+++ install.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-# Exibir o texto "MEMBER AREA" em formato grande
-
 # Instalar dependências
 sudo apt-get update > /dev/null 2>&1
 sudo apt-get install -y \
@@ -12,25 +10,8 @@ sudo apt-get install -y \
     software-properties-common \
     figlet > /dev/null 2>&1
 
-figlet "MEMBRIUMWL"
-
-# Adicionar chave GPG oficial do Docker
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add - > /dev/null 2>&1
-
-# Adicionar repositório do Docker
-sudo add-apt-repository \
-   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-   $(lsb_release -cs) \
-   stable" > /dev/null 2>&1   
-
-
-# Instalar o Docker Engine
-sudo apt-get update > /dev/null 2>&1
-sudo apt-get install -y docker-ce docker-ce-cli containerd.io > /dev/null 2>&1
-
-# Instalar o Docker Compose
-sudo curl -L "https://github.com/docker/compose/releases/download/v2.29.7/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose > /dev/null 2>&1
-sudo chmod +x /usr/local/bin/docker-compose > /dev/null 2>&1
+# Exibir o texto "MEMBER AREA" em formato grande
+figlet "MEMBER AREA"
 
 # Animação de "preparando"
 echo -n "Preparando"
@@ -42,11 +23,26 @@ while true; do
   done
 done &
 
-# Baixar o docker-compose.yml
-curl -L https://raw.githubusercontent.com/Celipi/member_area/main/docker-compose.yml -o docker-compose.yml
-
 # Finalizar a animação de "preparando"
+sleep 1
 kill %1
+
+# Adicionar chave GPG oficial do Docker
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add - > /dev/null 2>&1
+
+# Adicionar repositório do Docker
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable" > /dev/null 2>&1
+
+# Instalar o Docker Engine
+sudo apt-get update > /dev/null 2>&1
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io > /dev/null 2>&1
+
+# Instalar o Docker Compose
+sudo curl -L "https://github.com/docker/compose/releases/download/v2.29.7/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose > /dev/null 2>&1
+sudo chmod +x /usr/local/bin/docker-compose > /dev/null 2>&1
 
 # Animação de "baixando"
 echo -n "Baixando"
@@ -58,16 +54,20 @@ while true; do
   done
 done &
 
+# Baixar o docker-compose.yml
+curl -L https://raw.githubusercontent.com/Celipi/member_area/main/docker-compose.yml -o docker-compose.yml > /dev/null 2>&1
+
+# Finalizar a animação de "baixando"
+sleep 1
+kill %1
+
 # Criar a rede traefik_proxy (se não existir)
 if ! docker network inspect traefik_proxy >/dev/null 2>&1; then
-    sudo docker network create traefik_proxy
+    sudo docker network create traefik_proxy > /dev/null 2>&1
 fi
 
 # Criar o diretório letsencrypt (para os certificados SSL)
 mkdir -p letsencrypt
-
-# Finalizar a animação de "baixando"
-kill %1
 
 # Solicitar o domínio ao usuário
 echo "Digite o domínio que você deseja usar para o seu aplicativo (ex: meuaplicativo.com):"
@@ -94,9 +94,10 @@ while true; do
 done &
 
 # Iniciar a aplicação com o Docker Compose
-sudo docker-compose up -d
+sudo docker-compose up -d > /dev/null 2>&1
 
 # Finalizar a animação de "iniciando"
+sleep 1
 kill %1
 
 echo "Instalação concluída! Acesse seu aplicativo em https://$DOMINIO"
